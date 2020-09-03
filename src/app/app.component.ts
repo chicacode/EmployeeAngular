735ae8cbf823fd38ea9a92998358f4bfa150697e
gerihal <gerahernan.al@gmail.com>
2020-09-03 18:10:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'EmployeeAngular';
+  title = 'Employee-Managment Application System';
 }

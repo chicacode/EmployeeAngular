75331edd4b48490e25ff04f4c9a9683c6363a867
gerihal <gerahernan.al@gmail.com>
2020-09-22 17:58:08 +0200

    added routerLink to go to details

--- src/app/modules/inventory/inventory/inventory.module.ts
+++ src/app/modules/inventory/inventory/inventory.module.ts
@@ -5,6 +5,7 @@ import { InventoryRoutingModule } from './inventory-routing.module';
 
 import { CategoryComponent } from '../components/category/category/category.component';
 import { ProductListComponent } from '../components/product/components/product-list/product-list.component';
+import { ProductDetailComponent } from '../components/product/components/product-detail/product-detail.component';
 import { ProductAddComponent } from '../components/product/components/product-add/product-add.component';
 import { ProductUpdateComponent } from '../components/product/components/product-update/product-update.component';
 import { WarehouseComponent } from '../components/warehouse/warehouse/warehouse.component';
@@ -16,6 +17,7 @@ import { MovementsComponent } from '../components/movements/movements/movements.
   declarations: [
     CategoryComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ProductAddComponent,
     ProductUpdateComponent,
     WarehouseComponent,
@@ -31,6 +33,7 @@ import { MovementsComponent } from '../components/movements/movements/movements.
   exports: [
     CategoryComponent,
     ProductListComponent,
+    ProductDetailComponent,
     ProductAddComponent,
     ProductUpdateComponent,
     WarehouseComponent,

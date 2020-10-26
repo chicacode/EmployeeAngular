f6621fdc3511c7aac1609ad67dffe3d05a01e4ce
gerihal <gerahernan.al@gmail.com>
2020-10-26 17:37:16 +0100

    modifiying order detail c

--- src/app/modules/sales/components/order/order-detail/order-detail.component.ts
+++ src/app/modules/sales/components/order/order-detail/order-detail.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Order } from '../../../../../models/order';
+import { OrderService } from '../services/order.service';
 
 @Component({
   selector: 'app-order-detail',
@@ -6,10 +11,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-detail.component.scss']
 })
 export class OrderDetailComponent implements OnInit {
+  order: Order;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private orderService: OrderService,
+    private location: Location
+  ) { }
 
   ngOnInit(): void {
+    this.getOrder();
+  }
+
+  getOrder(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.orderService.getOrder(id)
+      .subscribe(order => this.order = order);
+  }
+
+  goBack(): void {
+    // navigate backward one step in the browsers history using Location service i injected previously
+    this.location.back();
   }
 
 }
